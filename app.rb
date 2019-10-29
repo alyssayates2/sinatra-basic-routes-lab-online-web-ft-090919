4e2d59d2b83bf67b90a34e1c3bdd85de46bf822b
Alyssa Yates <bosslys@icloud.com>
2019-10-29 22:45:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "Alyssa"
+  end
+
+  get '/hometown' do
+    "Frisco"
+  end
+
+  get '/favorite-song' do
+    "Sunflower"
+  end
 end
